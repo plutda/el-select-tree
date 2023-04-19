a4970cf2787016c0d6c031799138cf72f74369be
Doraemon <691841335@qq.com>
2023-04-19 10:17:01 +0800

    init

---
+++ dev/serve.vue
@@ -0,0 +1,33 @@
+<script>
+import Vue from 'vue';
+import { getData } from '../src/util'
+// import { Vue2SelectTreeSample } from '@/entry.esm';
+import { Vue2SelectTreeSample } from '../dist/vue2-select-tree.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+   Vue2SelectTreeSample,
+  },
+  data() {
+    return {
+      data: [],
+      select: []
+    }
+  },
+  methods: {
+  },
+  mounted() {
+    this.data = getData()
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue2-select-tree-sample
+      :value.sync="select"
+      :data="data"
+    />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,9 @@
+
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+/* eslint-disable import/prefer-default-export */
+export { default as Vue2SelectTreeSample } from './el-tree-select/index.vue';
